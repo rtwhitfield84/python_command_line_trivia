6a51aa668774faa784ce1338b4f70666496a3b3b
Rico <richardthomaswhitfield@gmail.com>
2017-02-09 17:05:48 -0600

    generating random key and getting response

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import requests
+import random
+from values import *
 
+def trivia():
+	question_key = random.randrange(9000,10000)
+
+	r = requests.get('https://nugacityquestions.firebaseio.com/{}.json'.format(question_key), headers=headers)
+
+	print(r.text)
+
+
+trivia()
