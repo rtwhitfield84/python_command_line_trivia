129ca8490019068ea7de53533de8cfa0cdbc501e
Rico <richardthomaswhitfield@gmail.com>
2017-02-09 22:43:34 -0600

    gameplay complete..with voices

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import requests
 import random
-from values import *
+import os
 
+# need to persist score
+# perhaps write to text file
 
-def trivia():
+def trivia(score):
 	question_key = random.randrange(9000,10000)
-	# score = 0
 	i = 1
 	r = requests.get('https://nugacityquestions.firebaseio.com/{}.json'
-					.format(question_key), headers=headers)
+					.format(question_key))
 
 	print(" ************************ \n "  
 		"Command Line Quiz Time! \n " 
 		"************************")
-	# print('Score ' + str(score))
-	print('Enter 9 to exit')
+	print('Score ' + str(score))
+	print('Enter 9 to exit \n')
 
 	for k,v in r.json().items():
 		if str(k) == 'answer':
@@ -23,14 +24,13 @@ def trivia():
 			question = v
 		if str(k) == 'choices':
 			choices = v 
-	print(question + '\n')
-	print(correct_answer)
-	print(choices)
+	print('Q: ' + question + '\n')
+
 
 	for i, j in enumerate(choices):
 		if str(j) == correct_answer:
 			correct_number = i + 1
-			print(correct_number)
+			print('Choices:')
 
 	for x,y in enumerate(choices):
 	    print(x + 1, y + '\n')
@@ -43,25 +43,34 @@ def trivia():
 			if user_answer==correct_number:
 				right()
 				break
+			elif user_answer==9:
+				quit()
+				break
 			elif user_answer!=correct_number:
 				wrong()
 				break
-			elif user_answer==9:
-				quit()
 			else:
 				print("Please enter a valid choice")
 		except ValueError:
 				print("Please enter a valid choice")
 	exit
 def right():
-	print("correct!")
-	# score += 100
-	trivia()
+	os.system("clear")
+	os.system('say -v "Good News" "correct" -r 300')
+
+	print("correct! \n")
+	trivia( 100)
+	
 def wrong():
-	print("wrong")
-	# score -= 100
-	trivia()
+	os.system("clear")
+	os.system('say -v Deranged "wrong"')
+	print("WRONG \n")
+	trivia( -100)
+	
 def quit():
 	exit
+def welcome():
+	os.system('say -v Zarvox "Welcome to the command line quiz" -r 300')
 
-trivia()
+welcome()
+trivia(0)
