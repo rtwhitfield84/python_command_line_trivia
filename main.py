f1f742d4abe48c78b443b1734e9fd1863c4cddc3
Rico <richardthomaswhitfield@gmail.com>
2017-02-09 21:49:10 -0600

    answer logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from values import *
 
 def trivia():
 	question_key = random.randrange(9000,10000)
-
+	i = 1
 	r = requests.get('https://nugacityquestions.firebaseio.com/{}.json'
 					.format(question_key), headers=headers)
 
@@ -14,14 +14,51 @@ def trivia():
 
 	for k,v in r.json().items():
 		if str(k) == 'answer':
-			answer = v
+			correct_answer = v
 		if str(k) == 'question':
 			question = v
 		if str(k) == 'choices':
 			choices = v 
-	print(question)
+	print(question + '\n')
+	print(correct_answer)
 	print(choices)
-	print(answer)
+
+	for i, j in enumerate(choices):
+		if str(j) == correct_answer:
+			correct_number = i + 1
+			print(correct_number)
+
+	for x,y in enumerate(choices):
+	    print(x + 1, y + '\n')
+
+
+	# while True:
+	# 	try:
+	# 		user_answer=int(input("Enter answer: "))
+
+	# 		if choice==1:
+	# 			kind()
+	# 			break
+	# 		elif choice==2:
+	# 			mean()
+	# 			break
+	# 		elif choice==3:
+	# 			weird()
+	# 			break
+	# 		elif choice==4:
+	# 			funny()
+	# 			break
+	# 		elif choice==5:
+	# 			fortune()
+	# 			break
+	# 		elif choice==6:
+	# 			quit()
+	# 			break
+	# 		else:
+	# 			print("Please enter a valid choice")
+	# 	except ValueError:
+	# 			print("Please enter a valid choice")
+	# exit
 
 
 trivia()
